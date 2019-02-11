8b50f18bf329bacd6ec4e4a9249ba020c66f2f62
Yuup92 <Y.R.J.M.vanEngelshoven@student.tudelft.nl>
2019-02-11 21:24:29 +0100

    Broadcast is working

--- Spanning_Tree/BasicNode.cc
+++ Spanning_Tree/BasicNode.cc
@@ -16,23 +16,17 @@ Define_Module(BasicNode);
 
 void BasicNode::initialize()
     {
-        // Initialize is called at the beginning of the simulation.
-        // To bootstrap the tic-toc-tic-toc process, one of the modules needs
-        // to send the first message. Let this be `tic'.
-
-        // Am I Tic or Toc?
-        if (strcmp("node0", getName()) == 0) {
+        // if (strcmp("node0", getName()) == 0) {
             // create and send first message on gate "out". "tictocMsg" is an
             // arbitrary string which will be the name of the message object.
             cMessage *msg = new cMessage("tictocMsg");
 
-            send(msg, "out", 0);
-
-//            for (int i = 0; i < 7; i++) {
-//                send(msg, "out", i);
-//            }
-
-        }
+            for (int i = 0; i < 6; i++) {
+                cMessage *copy = msg->dup();
+                send(copy, "out", i);
+            }
+            delete msg;
+        //}
     }
 
 void BasicNode::handleMessage(cMessage *msg)
